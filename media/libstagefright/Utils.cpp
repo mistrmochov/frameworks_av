b07bf4b2585d1f441c1393f0d9f53d558e633fe5
Michael Goffioul <michael.goffioul@gmail.com>
2019-06-02 18:55:36 -0400

    libstagefright: Extended media support via FFMPEG
     * Original work by Michael Chen - https://github.com/omxcodec
     * Original Kitkat port by Chih-Wei Huang / Android X86 project
     * Additional work up to Nougat by:
       - Steve Kondik
       - Ricardo Cerqueira
       - Keith Mok
       - Scott Mertz
       - Ethan Chen
       - Diogo Ferreira
     * Additional work up to Oreo/Pie/Q by:
       - Michael Goffioul
    
    Change-Id: I68f97e52a777ad0870cc29e045b7eb9c18f6d5b5

--- media/libstagefright/Utils.cpp
+++ media/libstagefright/Utils.cpp
@@ -44,6 +44,7 @@
 #include <media/AudioParameter.h>
 #include <system/audio.h>
 
+#include <media/stagefright/FFMPEGSoftCodec.h>
 #include <com_android_media_extractor_flags.h>
 
 // TODO : Remove the defines once mainline media is built against NDK >= 31.
@@ -789,6 +790,8 @@ static std::vector<std::pair<const char *, uint32_t>> stringMappings {
         { "manufacturer", kKeyManufacturer },
         { "title", kKeyTitle },
         { "year", kKeyYear },
+        // FFMEG
+        { "file-format", kKeyFileFormat },
     }
 };
 
@@ -837,6 +840,21 @@ static std::vector<std::pair<const char *, uint32_t>> int32Mappings {
         { "dvb-teletext-page-number", kKeyDvbTeletextPageNumber},
         { "profile", kKeyAudioProfile },
         { "level", kKeyAudioLevel },
+        // FFMPEG
+        { "bits-per-raw-sample", kKeyBitsPerRawSample },
+        { "block-align", kKeyBlockAlign },
+        { "codec-id", kKeyCodecId },
+        { "coded-sample-bits", kKeyCodedSampleBits },
+        { "divx-version", kKeyDivXVersion },
+        { "min-block-size", 'mibs' },
+        { "max-block-size", 'mabs' },
+        { "min-frame-size", 'mifs' },
+        { "max-frame-size", 'mafs' },
+        { "rv-version", kKeyRVVersion },
+        { "sample-format", kKeySampleFormat },
+        { "sample-rate", kKeySampleRate },
+        { "wma-version", kKeyWMAVersion },
+        { "wmv-version", kKeyWMVVersion },
     }
 };
 
@@ -855,6 +873,8 @@ static std::vector<std::pair<const char *, uint32_t>> bufferMappings {
         { "thumbnail-csd-hevc", kKeyThumbnailHVCC },
         { "slow-motion-markers", kKeySlowMotionMarkers },
         { "thumbnail-csd-av1c", kKeyThumbnailAV1C },
+        // FFMEG
+        { "raw-codec-specific-data", kKeyRawCodecSpecificData },
     }
 };
 
@@ -1622,7 +1642,14 @@ status_t convertMetaDataToMessage(
         msg->setBuffer("csd-2", buffer);
     }
 
+    FFMPEGSoftCodec::convertMetaDataToMessageFF(meta, &msg);
     *format = msg;
+
+#if 0
+    ALOGI("convertMetaDataToMessage from:");
+    meta->dumpToLog();
+    ALOGI("  to: %s", msg->debugString(0).c_str());
+#endif
 
     return OK;
 }
@@ -2351,8 +2378,10 @@ status_t convertMessageToMetaData(const sp<AMessage> &msg, sp<MetaData> &meta) {
     }
     // XXX TODO add whatever other keys there are
 
+    FFMPEGSoftCodec::convertMessageToMetaDataFF(msg, meta);
+
 #if 0
-    ALOGI("converted %s to:", msg->debugString(0).c_str());
+    ALOGI("convertMessageToMetaData from %s to:", msg->debugString(0).c_str());
     meta->dumpToLog();
 #endif
     return OK;
